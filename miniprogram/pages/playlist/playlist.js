ae5a8c799217ca0859da0ef607ef6f033f15cf4e
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-12-07 23:00:28 +0800

    基础页面架构

---
+++ miniprogram/pages/playlist/playlist.js
@@ -0,0 +1,99 @@
+// pages/palylist/palylist.js
+const MAX_LIMIT = 15
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperImgUrls: [{
+        url: 'http://p1.music.126.net/oeH9rlBAj3UNkhOmfog8Hw==/109951164169407335.jpg',
+      },
+      {
+        url: 'http://p1.music.126.net/xhWAaHI-SIYP8ZMzL9NOqg==/109951164167032995.jpg',
+      },
+      {
+        url: 'http://p1.music.126.net/Yo-FjrJTQ9clkDkuUCTtUg==/109951164169441928.jpg',
+      }
+    ],
+    // 轮播图
+    playlist: [], //歌单列表
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this._getPlaylist()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+    this.setData({
+      playlist:[]
+    })
+    this._getPlaylist()
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+    this._getPlaylist()
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {},
+  _getPlaylist() {
+    wx.showLoading({
+      title: '快马加鞭赶来啦',
+    })
+    wx.cloud.callFunction({
+      name: 'music',
+      data: {
+        start: this.data.playlist.length,
+        count: MAX_LIMIT
+      }
+    }).then((res) => {
+      // console.log(res)
+      this.setData({
+        playlist: this.data.playlist.concat(res.result.data)
+      })
+      wx.stopPullDownRefresh()
+      wx.hideLoading()
+    })
+  }
+})
