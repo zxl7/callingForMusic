30923f7d7a63f7516950884ee74e6a1574f753c2
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-12-10 23:50:20 +0800

    播放+光碟旋转动画+小指针

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -11,27 +11,32 @@ cloud.init()
 
 // 云函数入口函数
 exports.main = async(event, context) => {
-const app = new TcbRouter({
-  event
-})
-app.router('playlist', async(ctx, next) => {
-  ctx.body = await cloud.database().collection('playlist')
-    .skip(event.start)
-    .limit(event.count)
-    .orderBy('createTime', 'desc')
-    .get()
-    .then((res) => {
-      return res
-    })
-})
+  const app = new TcbRouter({
+    event
+  })
+  app.router('playlist', async(ctx, next) => {
+    ctx.body = await cloud.database().collection('playlist')
+      .skip(event.start)
+      .limit(event.count)
+      .orderBy('createTime', 'desc')
+      .get()
+      .then((res) => {
+        return res
+      })
+  })
 
-app.router('musiclist', async(ctx, next) => {
-  ctx.body = await rp(BASE_URL + '/playlist/detail?id=' + parseInt(event.playlistId))
-    .then((res) => {
-      return JSON.parse(res)
-    })
-})
-return app.serve()
+  app.router('musiclist', async(ctx, next) => {
+    ctx.body = await rp(BASE_URL + '/playlist/detail?id=' + parseInt(event.playlistId))
+      .then((res) => {
+        return JSON.parse(res)
+      })
+  })
 
-
+  app.router('musicUrl', async(ctx, next) => {
+    ctx.body = await rp(BASE_URL + `/song/url?id=${event.musicId}`)
+      .then((res) => {
+        return res
+      })
+  })
+  return app.serve()
 }
--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,13 +1,18 @@
 let musiclist = []
 // 正在播放歌曲的index
 let nowPlayingIndex = 0
+
+// 获取全局唯一的背景音频管理器
+const backgroundAudioManager = wx.getBackgroundAudioManager()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    picUrl: ''
+    picUrl: '',
+    isPlaying: false //false表示不播放
 
   },
 
@@ -18,9 +23,11 @@ Page({
     console.log(options)
     musiclist = wx.getStorageSync('musiclist')
     nowPlayingIndex = options.index
-    this._loadMusicDetail()
+    this._loadMusicDetail(options.musicId)
   },
-  _loadMusicDetail() {
+  // 拿到歌曲信息
+  _loadMusicDetail(musicId) {
+    backgroundAudioManager.stop()
     let music = musiclist[nowPlayingIndex]
     console.log(music)
     wx.setNavigationBarTitle({
@@ -28,57 +35,61 @@ Page({
     })
 
     this.setData({
-      picUrl: music.al.picUrl
+      picUrl: music.al.picUrl,
+      isPlaying: false
     })
 
+
+    wx.showLoading({
+      title: '歌曲加载中···',
+    })
+
+    wx.cloud.callFunction({
+      name: 'music',
+      data: {
+        musicId,
+        $url: 'musicUrl'
+      }
+    }).then((res) => {
+      console.log(JSON.parse(res.result))
+      let result = JSON.parse(res.result)
+      backgroundAudioManager.src = result.data[0].url
+      backgroundAudioManager.title = music.name
+      backgroundAudioManager.coverImgUrl = music.al.picUrl
+      backgroundAudioManager.singer = music.ar[0].name
+      backgroundAudioManager.epname = music.al.name
+      this.setData({
+        isPlaying: true
+      })
+      wx.hideLoading()
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
+  // 控制切换
+  togglePlaying() {
+    if (this.data.isPlaying) {
+      backgroundAudioManager.pause()
+    } else {
+      backgroundAudioManager.play()
+    }
+    this.setData({
+      isPlaying: !this.data.isPlaying
+    })
+  },
+  onPrev() {
+    nowPlayingIndex--
+    if (nowPlayingIndex < 0) {
+      nowPlayingIndex = musiclist.length - 1
+    }
+    this._loadMusicDetail(musiclist[nowPlayingIndex].id)
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
+  onNext() {
+    nowPlayingIndex++
+    if (nowPlayingIndex === musiclist.length - 1) {
+      nowPlayingIndex = 0
+    }
+    this._loadMusicDetail(musiclist[nowPlayingIndex].id)
 
   }
 })
